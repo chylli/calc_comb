b3892b8acf945f0934ca76294abf642b5e4e68a1
chylli-binary <chylli@binary.com>
2019-07-24 23:51:20 +0800

    no sum

--- Calc.pm
+++ Calc.pm
@@ -4,7 +4,7 @@ use warnings;
 use feature qw(state say);
 use Moo;
 use Data::Dumper;
-
+our $DEBUG = 0;
 has data => (is => 'ro', required => 1);
 has total => (is => 'ro', required => 1);
 has sorted_data => (is => 'lazy');
@@ -12,55 +12,35 @@ sub _build_sorted_data {
   my $self = shift;
   return [reverse sort @{$self->data}];
 }
-has sums => (is => 'lazy');
-sub _build_sums {
-  my $self = shift;
-  my $data = $self->sorted_data;
-
-  my @sums;
-  for my $j (0..$#$data) {
-    my $k = $#$data - $j;
-    my $result = $sums[0];
-    $result += $data->[$k];
-    unshift @sums, $result;
-  }
-  return @sums;
-}
 sub calc{
   my $self = shift;
   return [$self->do_calc($self->total, 0, 0)];
 }
 sub do_calc{
   my ($self, $total, $i, $layer ) = @_;
-  $DB::single = 1;
   my $data = $self->sorted_data;
-  say "." x $layer, $data->[$i] // '';
-  say "$total, $i";
+  say "." x $layer, $data->[$i] // '' if $DEBUG;
+  say "$total, $i" if $DEBUG;
+
+  # the numbers before this one is the answer, no need to check more
   return ([]) if $total == 0;
-  return () if $i > $#$data;
-  # bad comb
-  #return () if sum($i+1) < $total - $data->[$i];
-  return () if $total < 0;
+  #no result
+  return () if $i > $#$data || $total < 0;
 
   my @total_result;
-    # find it! return $i or $data$[i];
- # push @total_result, [$data->[$i]] if $total == $data->[$i];
 
   # the results that include this one
   my @result = $self->do_calc($total - $data->[$i], $i+1, $layer+1);
   for my $r (@result){
-    # unshift $data->[$i] or [$i];
+    # return data or index
     unshift @$r, $data->[$i];
   }
   push @total_result, @result;
-  
+
   @result = $self->do_calc($total, $i+1, $layer+1);
   push @total_result, @result;
 
   return @total_result;
-}
-
-sub calc_sum{
 }
 
 1;
