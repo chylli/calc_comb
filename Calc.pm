7cdeed5423c9b93f0883b22c901fde98d233d10c
chylli-binary <chylli@binary.com>
2019-07-24 22:38:44 +0800

    tmp

--- Calc.pm
+++ Calc.pm
@@ -2,47 +2,62 @@ package Calc;
 use strict;
 use warnings;
 use feature qw(state say);
+use Moo;
 use Data::Dumper;
 
+has data => (is => 'ro', required => 1);
+has total => (is => 'ro', required => 1);
+has sorted_data => (is => 'lazy');
+sub _build_sorted_data {
+  my $self = shift;
+  return [reverse sort @{$self->data}];
+}
+has sums => (is => 'lazy');
+sub _build_sums {
+  my $self = shift;
+  my $data = $self->sorted_data;
+
+  my @sums;
+  for my $j (0..$#$data) {
+    my $k = $#$data - $j;
+    my $result = $sums[0];
+    $result += $data->[$k];
+    unshift @sums, $result;
+  }
+  return @sums;
+}
 sub calc{
-  my ($total, $i, $layer ) = @_;
-  say "." x $layer, "$data[$i]";
+  my $self = shift;
+  return [$self->do_calc($self->total, 0, 0)];
+}
+sub do_calc{
+  my ($self, $total, $i, $layer ) = @_;
+  my $data = $self->sorted_data;
+  say "." x $layer, "$data->[$i]";
   # find it! return $i or $data$[i];
-  return ([$data[$i]]) if $total == $data[$i];
-  return () if $i > $#data;
+  return ([$data->[$i]]) if $total == $data->[$i];
+  return () if $i > $#$data;
   # bad comb
-  #return () if sum($i+1) < $total - $data[$i];
+  #return () if sum($i+1) < $total - $data->[$i];
   return () if $total < 0;
 
   my @total_result;
-  for my $j ($i+1 .. $#data){
-      my @result = calc($total - $data[$i], $i+1, $layer+1);
+  for my $j ($i+1 .. $#$data){
+      my @result = $self->do_calc($total - $data->[$i], $i+1, $layer+1);
       for my $r (@result){
-        # unshift $data[$i] or [$i];
-        unshift @$r, $data[$i];
+        # unshift $data->[$i] or [$i];
+        unshift @$r, $data->[$i];
       }
       push @total_result, @result;
 
-      @result = calc($total, $i+1, $layer+1);
+      @result = $self->do_calc($total, $i+1, $layer);
       push @total_result, @result;
 
     }
   return @total_result;
 }
 
-sub sum{
-  my $i = shift;
-  state @sum = ();
-  if(@sum == 0){
-    for my $j (0..$#data) {
-      my $k = $#data - $j;
-          my $result = $sum[0];
-          $result += $data[$k];
-          unshift @sum, $result;
-        }
-
-  }
-  return $sum[$i];
+sub calc_sum{
 }
 
 1;
