175e3ff8a7f62f35e257c70a95f38d6bbd50aacc
chylli-binary <chylli@binary.com>
2019-07-24 21:52:36 +0800

    init

---
+++ Calc.pm
@@ -0,0 +1,48 @@
+package Calc;
+use strict;
+use warnings;
+use feature qw(state say);
+use Data::Dumper;
+
+sub calc{
+  my ($total, $i, $layer ) = @_;
+  say "." x $layer, "$data[$i]";
+  # find it! return $i or $data$[i];
+  return ([$data[$i]]) if $total == $data[$i];
+  return () if $i > $#data;
+  # bad comb
+  #return () if sum($i+1) < $total - $data[$i];
+  return () if $total < 0;
+
+  my @total_result;
+  for my $j ($i+1 .. $#data){
+      my @result = calc($total - $data[$i], $i+1, $layer+1);
+      for my $r (@result){
+        # unshift $data[$i] or [$i];
+        unshift @$r, $data[$i];
+      }
+      push @total_result, @result;
+
+      @result = calc($total, $i+1, $layer+1);
+      push @total_result, @result;
+
+    }
+  return @total_result;
+}
+
+sub sum{
+  my $i = shift;
+  state @sum = ();
+  if(@sum == 0){
+    for my $j (0..$#data) {
+      my $k = $#data - $j;
+          my $result = $sum[0];
+          $result += $data[$k];
+          unshift @sum, $result;
+        }
+
+  }
+  return $sum[$i];
+}
+
+1;
---
+++ test.t
@@ -0,0 +1,6 @@
+use strict;
+use warnings;
+use Test::More;
+use Calc;
+
+is_deeply(Calc::calc())
